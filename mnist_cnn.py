ba35d389b50448db60b608a562d05e4065c49061
Ma <jma7@BCBMC02MG1WJF6T.zyxel.com>
2017-10-27 00:11:06 -0500

    try ann

--- mnist_cnn.py
+++ mnist_cnn.py
@@ -1,11 +1,11 @@
 from tensorflow.examples.tutorials.mnist import input_data
 import tensorflow as tf
+from util import *
 
 
 
 class logistic(object):
 	def __init__(self,mnist):
-	
 		self.mnist=mnist
 		self.x=tf.placeholder(tf.float32,shape=[None,784])
 		self.T=tf.placeholder(tf.float32,shape=[None,10])
@@ -14,15 +14,15 @@ class logistic(object):
 
 
 	def fit(self):
-		y=self.forward(self.x)
-		cross_entropy=tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=self.T,logits=y))
+		self.y=self.forward(self.x)
+		cross_entropy=tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=self.T,logits=self.y))
 		train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
 		with tf.Session() as sess:
 			sess.run(tf.global_variables_initializer())
 			for _ in range(1000):
 			    batch = self.mnist.train.next_batch(100)
 			    sess.run(train_step, feed_dict={self.x: batch[0], self.T: batch[1]})
-			correct_prediction = tf.equal(tf.argmax(y,1), tf.argmax(self.T,1))
+			correct_prediction = tf.equal(tf.argmax(self.y,1), tf.argmax(self.T,1))
 			accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 			print(accuracy.eval(feed_dict={self.x: self.mnist.test.images, self.T: self.mnist.test.labels}))
 
@@ -35,10 +35,98 @@ class logistic(object):
 	def forward(self,x):
 		return tf.matmul(x,self.W) + self.b
 
+class HiddenLayer(object):
+	def __init__(self,M1,M2,id):
+		self.id=id
+		self.M1=M1
+		self.M2=M2
+		w,b=init_weight_and_bias(M1,M2)
+		self.W=tf.Variable(w.astype(np.float32))
+		self.b=tf.Variable(w.astype(np.float32))
+		self.params=[self.W,self.b]
+
+	def forward(self,X):
+		print("hshape",self.W)
+		return tf.nn.relu(tf.matmul(X,self.W)+self.b)
+
+class ANN(object):
+	def __init__(self,hidden_layer_sizes,mnist):
+		self.hidden_layer_sizes=hidden_layer_sizes
+		self.x=tf.placeholder(tf.float32,shape=[None,784])
+		self.T=tf.placeholder(tf.float32,shape=[None,10])
+		self.mnist=mnist
+
+	def fit(self,learning_rate=10e-3, mu=0.99, decay=0.999, reg=10e-3, epochs=200, batch_sz=100, show_fig=False):
+		N,D=self.mnist.test.images.shape
+		print("input",N,D)
+		self.hidden_layers=[]
+		M1=D
+		count=0
+		for M2 in self.hidden_layer_sizes:
+			h=HiddenLayer(M1,M2,count)
+			print("hidden",M1,M2)
+			M1=M2
+			count+=1
+			self.hidden_layers.append(h)
+		K=10
+		W,b=init_weight_and_bias(M1,K)
+		print("output",M1,K)
+		self.W = tf.Variable(W.astype(np.float32))
+		self.b = tf.Variable(b.astype(np.float32))
+		self.params = [self.W, self.b]
+		for h in self.hidden_layers:
+			self.params += h.params
+		self.y=self.forward(self.x)
+
+		rcost = reg*sum([tf.nn.l2_loss(p) for p in self.params])
+		cost=tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=self.y,labels=self.T))+rcost
+		train_op = tf.train.RMSPropOptimizer(learning_rate, decay=decay, momentum=mu).minimize(cost)
+
+		init = tf.global_variables_initializer()
+		
+		with tf.Session() as sess:
+			sess.run(init)
+			for _ in range(epochs):
+
+				batch = self.mnist.train.next_batch(100)
+				sess.run(train_step, feed_dict={self.x: batch[0], self.T: batch[1]})
+			correct_prediction = tf.equal(tf.argmax(self.y,1), tf.argmax(self.T,1))
+			accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+			print(accuracy.eval(feed_dict={self.x: self.mnist.test.images, self.T: self.mnist.test.labels}))
+
+
+
+	def forward(self,X):
+		Z=X
+		for h in self.hidden_layers:
+			print(Z)
+			Z=h.forward(Z)
+			print(h.M1,h.M2,Z)
+
+		return tf.matmul(Z,self.W)+self.b
+
+# class cnn(object):
+# 	def __init__(self,mnist):
+# 		self.mnist=mnist
+# 		self.x=tf.placeholder(tf.float32,shape=[None,784])
+# 		self.T=tf.placeholder(tf.float32,shape=[None,10])
+# 		self.W=tf.Variable(tf.zeros([784,10]))
+# 		self.b=tf.Variable(tf.zeros([10]))
+
+# 	def fit(self):
+
+
+
+
+# 	def forward(self,x):
+
+
+
 
 
 mnist=input_data.read_data_sets('MNIST_data',one_hot=True)
-model=logistic(mnist)
+# model=logistic(mnist)
+model=ANN([50,25],mnist)
 model.fit()
 
 
